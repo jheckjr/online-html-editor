a5de147e445f9c086d8f144ad410d7aa32be3dc8
jheckjr <joel.heckjr@gmail.com>
2017-04-02 20:48:07 -0400

    Add simple text area and post to server

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/editor', function(req, res) {
+  res.render('editor');
+});
+
+router.post('/updatehtml', function(req, res) {
+  var updatedHtml = req.body.newhtml;
+  console.log("Received HTML:" + updatedHtml);
+  res.redirect("editor");
+});
+
 module.exports = router;
