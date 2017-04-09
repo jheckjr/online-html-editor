c6d303238036797f60b80573791a76d9c006ae47
jheckjr <joel.heckjr@gmail.com>
2017-04-08 20:05:45 -0400

    Add follow function

--- app/public/javascripts/client-change-set.js
+++ app/public/javascripts/client-change-set.js
@@ -4,8 +4,39 @@
 function ChangeSet(startLen) {
   this.startLen = startLen;
   this.endLen = startLen;
-  this.ops = [newOp(noOp, startLen)];
+  this.ops = [];
+  if (startLen > 0) {
+    this.ops = [newOp(OpEnum.EQUAL, startLen)];
+  }
   this.changeText = '';
+
+  this.compress = function() {
+    let newOps = [];
+    let newEndLen = 0;
+    for (let idx = 0; idx < this.ops.length; idx++) {
+      let jumpIdx = idx;
+      let len = 0;
+
+      // Collect consecutive operations with same type
+      while (jumpIdx < this.ops.length && this.ops[jumpIdx].op === this.ops[idx].op) {
+        len += this.ops[jumpIdx].len;
+        jumpIdx += 1;
+      }
+
+      // Add operations to list and jump the index
+      newOps.push(newOp(this.ops[idx].op, len));
+      if (this.ops[idx].op !== OpEnum.REMOVE) {
+        newEndLen += len;
+      }
+      idx = jumpIdx - 1;
+    }
+
+    // Use the compressed version if different
+    if (newOps.length > 0) {
+      this.ops = newOps;
+      this.endLen = newEndLen;
+    }
+  };
 }
 
 let clientCS = {
@@ -14,9 +45,11 @@ let clientCS = {
   y: new ChangeSet(0) // Unsubmitted changes
 };
 
-let addOp = '+';
-let removeOp = '-';
-let noOp = '=';
+let OpEnum = {
+  ADD: '+',
+  REMOVE: '-',
+  EQUAL: '='
+};
 
 function newOp(op, opLength) {
   return {
@@ -43,7 +76,7 @@ function getCSFromCM(changeObj, content) {
 
   // Add new line operation
   if (changeObj.text.length == 2) {
-    cmCS.ops.push(newOp(addOp, 1));
+    cmCS.ops.push(newOp(OpEnum.ADD, 1));
     cmCS.changeText += '\n';
     numRemaining -= 1;
   }
@@ -51,19 +84,19 @@ function getCSFromCM(changeObj, content) {
   // Remove characters
   let numToChange = changeObj.removed[0].length;
   if (numToChange > 0) {
-    cmCS.ops.push(newOp(removeOp, numToChange));
+    cmCS.ops.push(newOp(OpEnum.REMOVE, numToChange));
   }
 
   // Add new characters
   numToChange = changeObj.text[0].length;
   if (numToChange > 0) {
-    cmCS.ops.push(newOp(addOp, numToChange));
+    cmCS.ops.push(newOp(OpEnum.ADD, numToChange));
     cmCS.changeText += changeObj.text[0];
     numRemaining -= numToChange;
   }
   // Add remaining characters
   if (numRemaining > 0) {
-    cmCS.ops.push(newOp(noOp, numRemaining));
+    cmCS.ops.push(newOp(OpEnum.EQUAL, numRemaining));
   }
 
   return cmCS;
@@ -95,5 +128,157 @@ function getCSFromCM(changeObj, content) {
  * Apply a change from the editor to the unsubmitted changeset
  */
 function applyCSFromCM(cmCS) {
-  clientCS.compose(cmCS);
+  clientCS.y = composeCS(clientCS.y, cmCS);
+}
+
+/*
+ * Compose two changesets together
+ */
+function composeCS(csA, csB) {
+  // Check that the lengths match
+  if (csA.endLen != csB.startLen) {
+    return new ChangeSet(0);
+  }
+
+  // Init new changeset
+  let newCS = new ChangeSet(0);
+  newCS.startLen = csA.startLen;
+  newCS.endLen = csB.endLen;
+
+  let changeTextAIdx = 0; // index of next text in csA changeText
+  let changeTextBIdx = 0; // index of next text in csB changeText
+  let opsAIdx = 0; // index of next operation in csA
+  // Apply each operation in csB to csA
+  for (let opsBIdx = 0; opsBIdx < csB.ops.length; opsBIdx++) {
+    let opB = csB.ops[opsBIdx].op;
+    let lenB = csB.ops[opsBIdx].len;
+
+    if (opB === OpEnum.EQUAL || opB === OpEnum.REMOVE) {
+      while (lenB > 0) {
+        // Keep removes from csA first
+        if (csA.ops[opsAIdx].op === OpEnum.REMOVE) {
+          newCS.ops.push(csA.ops[opsAIdx]);
+          opsAIdx += 1;
+        } else {
+          // If more change in csB than csA
+          if (lenB >= csA.ops[opsAIdx].len) {
+            lenB -= csA.ops[opsAIdx].len;
+            if (opB === OpEnum.EQUAL) {
+              // Keep csA operation if opB is equal
+              newCS.ops.push(csA.ops[opsAIdx]);
+            } else if (csA.ops[opsAIdx].op === OpEnum.EQUAL){
+              // Keep csB remove operation if csA is equal
+              newCS.ops.push(newOp(OpEnum.REMOVE, csA.ops[opsAIdx].len));
+            }
+
+            // Move csA changeText index if add operation
+            if (csA.ops[opsAIdx].op === OpEnum.ADD) {
+              if (opB === OpEnum.EQUAL) {
+                newCS.changeText += csA.changeText.substr(changeTextAIdx, csA.ops[opsAIdx].len);
+              }
+              changeTextAIdx += csA.ops[opsAIdx].len;
+            }
+
+            opsAIdx += 1;
+          } else {
+            // Less change in csB than csA
+            if (csA.ops[opsAIdx].op === OpEnum.EQUAL) {
+              // Keep csB operation if equal operation in csA
+              newCS.ops.push(csB.ops[opsBIdx]);
+            } else if (csA.ops[opsAIdx].op === OpEnum.ADD) {
+              // Keep csA add operation if equal operation in csB
+              if (opB === OpEnum.EQUAL) {
+                newCS.ops.push(newOp(OpEnum.ADD, lenB));
+                newCS.changeText += csA.changeText.substr(changeTextAIdx, lenB);
+              }
+              changeTextAIdx += lenB;
+            }
+
+            csA.ops[opsAIdx].len -= lenB;
+            lenB = 0;
+          }
+        }
+      }
+    } else if (opB === OpEnum.ADD) {
+      // If adding, push the add operation
+      newCS.ops.push(csB.ops[opsBIdx]);
+      newCS.changeText += csB.changeText.substr(changeTextBIdx, lenB);
+      changeTextBIdx += lenB;
+    } else {
+      console.err("Unknown operation: " + opB);
+    }
+  }
+
+  while (opsAIdx < csA.ops.length) {
+    if (csA.ops[opsAIdx].op === OpEnum.REMOVE) {
+      newCS.ops.push(csA.ops[opsAIdx]);
+    }
+
+    opsAIdx += 1;
+  }
+
+  newCS.compress();
+
+  return newCS;
+}
+
+function follow(csA, csB) {
+  if (csA.startLen != csB.startLen) {
+    console.err("Changeset start lengths are different for merge.");
+    return new ChangeSet(0);
+  }
+
+  // Init new changeset
+  newCS = new ChangeSet(0);
+  newCS.startLen = csA.startLen;
+
+  let opsAIdx = 0;
+  let opsBIdx = 0;
+  let changeTextAIdx = 0;
+  let changeTextBIdx = 0;
+  let opLenA = 0;
+  let opLenB = 0;
+  let left = 0;
+  while (opsAIdx < csA.ops.length && opsBIdx < csB.ops.length) {
+    // Insertions in setA become retained
+    if (csA.ops[opsAIdx].op === OpEnum.ADD) {
+      newCS.ops.push(newOp(OpEnum.EQUAL), csA.ops[opsAIdx].len);
+      changeTextAIdx += csA.ops[opsAIdx].len;
+      opsAIdx += 1;
+      opLenA += csA.ops[opsAIdx].len;
+    }
+    // Insertions in setB stay as insertions
+    if (csB.ops[opsBIdx].op === OpEnum.ADD) {
+      newCS.ops.push(csB.ops[opsBIdx]);
+      changeTextBIdx += csB.ops[opsBIdx].len;
+      opsBIdx += 1;
+      opLenB += csB.ops[opsBIdx].len;
+    }
+
+    if (csA.ops[opsAIdx].op !== OpEnum.ADD && csB.ops[opsBIdx].op === OpEnum.ADD) {
+      let right = Math.min(opLenA, opLenB);
+      if (csA.ops[opsAIdx].op === OpEnum.EQUAL) {
+        newCS.ops.push(newOp(csB.ops[opsBIdx].op, right - left + 1));
+      }
+
+      if (opLenA === right) {
+        opsAIdx += 1;
+        if (csA.ops[opsAIdx].op !== OpEnum.ADD) {
+          opLenA += csA.ops[opsAIdx].len;
+        }
+      }
+      if (opLenB === right) {
+        opsBIdx += 1;
+        if (csB.ops[opsBIdx].op !== OpEnum.ADD) {
+          opLenB += csB.ops[opsBIdx].len;
+        }
+      }
+    }
+    left = right + 1;
+  }
+
+  newCS.changeText = csB.changeText;
+  newCS.compress();
+
+  return newCS;
 }
